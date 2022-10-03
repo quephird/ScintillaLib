887557f15b5ddf82af043f79349528bf9c53e04b
quephird <danielle_kefford@hotmail.com>
2022-10-03 11:50:39 -0700

    Improved code a little bit.

--- Sources/ScintillaLib/ImplicitSurface.swift
+++ Sources/ScintillaLib/ImplicitSurface.swift
@@ -32,14 +32,10 @@ public class ImplicitSurface: Shape {
         var tPrev: Double = 0.0
         var t: Double
         var iterations = 0
-        while true {
+        while iterations <= MAX_ITERATIONS_NEWTON {
             t = tPrev - ft(tPrev)/fPrimeT(tPrev)
             if abs(tPrev - t) < EPSILON {
                 return [Intersection(t, self)]
-            }
-
-            if iterations == MAX_ITERATIONS_NEWTON {
-                break
             }
 
             tPrev = t
@@ -50,9 +46,9 @@ public class ImplicitSurface: Shape {
     }
 
     override func localNormal(_ localPoint: Tuple4) -> Tuple4 {
-        let gradFx = (f(localPoint.x + EPSILON, localPoint.y, localPoint.z) - f(localPoint.x, localPoint.y, localPoint.z))/EPSILON
-        let gradFy = (f(localPoint.x, localPoint.y + EPSILON, localPoint.z) - f(localPoint.x, localPoint.y, localPoint.z))/EPSILON
-        let gradFz = (f(localPoint.x, localPoint.y, localPoint.z + EPSILON) - f(localPoint.x, localPoint.y, localPoint.z))/EPSILON
+        let gradFx = (f(localPoint.x + EPSILON, localPoint.y, localPoint.z) - f(localPoint.x, localPoint.y, localPoint.z))
+        let gradFy = (f(localPoint.x, localPoint.y + EPSILON, localPoint.z) - f(localPoint.x, localPoint.y, localPoint.z))
+        let gradFz = (f(localPoint.x, localPoint.y, localPoint.z + EPSILON) - f(localPoint.x, localPoint.y, localPoint.z))
 
         return vector(gradFx, gradFy, gradFz).normalize()
     }
