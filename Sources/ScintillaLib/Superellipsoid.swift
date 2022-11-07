aba2fd6e20cd9d4d94f1bc46438e862654b7e614
quephird <danielle_kefford@hotmail.com>
2022-11-07 12:49:20 -0800

    Whoops... I forgot to update this branch to be consistent with the new ImplicitSurface API.

--- Sources/ScintillaLib/Superellipsoid.swift
+++ Sources/ScintillaLib/Superellipsoid.swift
@@ -11,13 +11,14 @@ public class Superellipsoid: Shape {
     var underlyingImplicitSurface: ImplicitSurface
 
     public init(_ e: Double, _ n: Double) {
-        let boundingBox = ((-1.0 - EPSILON, -1.0 - EPSILON, -1.0 - EPSILON), (1.0 + EPSILON, 1.0 + EPSILON, 1.0 + EPSILON))
+        let bottomLeftFront = (-1.0 - EPSILON, -1.0 - EPSILON, -1.0 - EPSILON)
+        let topRightBack = (1.0 + EPSILON, 1.0 + EPSILON, 1.0 + EPSILON)
 
         func f(_ x: Double, _ y: Double, _ z: Double) -> Double {
             pow(pow(abs(x), 2.0/e) + pow(abs(y), 2.0/e), e/n) + pow(abs(z), 2.0/n) - 1.0
         }
 
-        let underlyingImplicitSurface = ImplicitSurface(boundingBox, f)
+        let underlyingImplicitSurface = ImplicitSurface(bottomLeftFront, topRightBack, f)
         self.underlyingImplicitSurface = underlyingImplicitSurface
     }
 
