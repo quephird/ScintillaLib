be923739e579369e95081c77046746a1185b1aff
quephird <danielle_kefford@hotmail.com>
2023-10-06 18:24:25 -0700

    No longer need additional files in Examples module.

--- Examples/BallWithAreaLight/BallWithAreaLight.swift
+++ Examples/BallWithAreaLight/BallWithAreaLight.swift
@@ -7,21 +7,22 @@
 
 import ScintillaLib
 
-//@main
-//struct BallWithAreaLight: ScintillaApp {
-//    var body: World {
-//        AreaLight(
-//            Point(-5, 5, -5),
-//            Vector(2, 0, 0), 10,
-//            Vector(0, 2, 0), 10)
-//        Camera(400, 400, PI/3, .view(
-//            Point(0, 2, -5),
-//            Point(0, 1, 0),
-//            Vector(0, 1, 0)))
-//        Sphere()
-//            .translate(0, 1, 0)
-//            .material(.solidColor(1, 0, 0))
-//        Plane()
-//            .material(.solidColor(1, 1, 1))
-//    }
-//}
+@available(macOS 12.0, *)
+@main
+struct BallWithAreaLight: ScintillaApp {
+    var world: World {
+        AreaLight(
+            Point(-5, 5, -5),
+            Vector(2, 0, 0), 10,
+            Vector(0, 2, 0), 10)
+        Camera(400, 400, PI/3, .view(
+            Point(0, 2, -5),
+            Point(0, 1, 0),
+            Vector(0, 1, 0)))
+        Sphere()
+            .translate(0, 1, 0)
+            .material(.solidColor(1, 0, 0))
+        Plane()
+            .material(.solidColor(1, 1, 1))
+    }
+}
