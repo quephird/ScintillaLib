16aeb98f75670da6f7b4a6069b0dc810b25848cd
quephird <danielle.kefford@gmail.com>
2025-04-18 14:43:12 -0700

    Updated example sketch.

--- Examples/FocalBlur/FocalBlur.swift
+++ Examples/FocalBlur/FocalBlur.swift
@@ -12,31 +12,37 @@ import ScintillaLib
 struct FocalBlurExample: ScintillaApp {
     var world = World {
         Camera(
-            width: 400,
-            height: 400,
+            width: 600,
+            height: 600,
             viewAngle: PI/3,
-            from: Point(0, 3, -4),
+            from: Point(0, 6, -8),
             to: Point(0, 0, 0),
             up: Vector(0, 1, 0))
         .focalBlur(FocalBlur(
-            focalDistance: 5.0,
+            focalDistance: 10.0,
             aperture: 0.2,
             samples: 50))
         PointLight(position: Point(-10, 10, -10))
         Sphere()
             .material(.uniform(1, 0, 0))
+            .translate(-4.5, 0, 4.5)
         Sphere()
-            .material(.uniform(0, 1, 0))
-            .translate(-3, 0, 4)
-        Sphere()
-            .material(.uniform(0, 0, 1))
-            .translate(3, 0, 4)
+            .material(.uniform(1, 0.5, 0))
+            .translate(-3.0, 0, 3.0)
         Sphere()
             .material(.uniform(1, 1, 0))
-            .translate(-2, 0, -2)
+            .translate(-1.5, 0, 1.5)
         Sphere()
-            .material(.uniform(0, 1, 1))
-            .translate(2, 0, -2)
+            .material(.uniform(0, 1, 0))
+        Sphere()
+            .material(.uniform(0, 0, 1))
+            .translate(1.5, 0, -1.5)
+        Sphere()
+            .material(.uniform(0.25, 0, 1))
+            .translate(3, 0, -3)
+        Sphere()
+            .material(.uniform(0.5, 0, 1))
+            .translate(4.5, 0, -4.5)
         Plane()
             .translate(0.0, -1.0, 0.0)
     }
